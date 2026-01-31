e135c961086da800ec4151470439fe8c89b79aab
vaishnavigowda2031 <vaishnavi2004r@gmail.com>
2026-01-31 18:47:47 +0530

    Refactor MongoDB connection and CORS setup

--- server.js
+++ server.js
@@ -5,16 +5,21 @@ const cors = require("cors");
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
-app.use(cors({ origin: process.env.CORS_ORIGIN || "*" }));
+
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
-
+// 👇 THIS LINE IS CRITICAL
 app.use("/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
