9f341f0e0d789c6c787cd1365e88d86b30a87e48
vaishnavigowda2031 <vaishnavi2004r@gmail.com>
2026-01-31 18:46:49 +0530

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// test route
+router.get("/", (req, res) => {
+  res.json({ message: "Tasks route working" });
+});
+
+module.exports = router;
 
