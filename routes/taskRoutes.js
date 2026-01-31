93254b26afd9174107980a3b0c9eedc1eb48aa01
vaishnavigowda2031 <vaishnavi2004r@gmail.com>
2026-01-31 21:01:31 +0530

    Implement task creation route
    Added a POST route to create a new task.

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Task = require("../models/task");
 
-// test route
-router.get("/", (req, res) => {
-  res.json({ message: "Tasks route working" });
+// CREATE TASK
+router.post("/", async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    const savedTask = await task.save();
+    res.status(201).json(savedTask);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 module.exports = router;
-
