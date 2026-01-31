5b634f8cdb0aa598bdbbcdf4c846f52d70af47fe
vaishnavigowda2031 <vaishnavi2004r@gmail.com>
2026-01-31 17:06:16 +0530

    Delete Task.js file
    Remove Task schema and model definition.

--- Task.js
+++ Task.js
@@ -1,9 +1,1 @@
-const mongoose = require("mongoose");
 
-const TaskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  status: { type: String, default: "Pending" }
-}, { timestamps: true });
-
-module.exports = mongoose.model("Task", TaskSchema);
